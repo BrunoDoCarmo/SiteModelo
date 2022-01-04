cd8e8dcb0627af32130c68dfc7bed834abaf912d
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-01-04 17:50:16 -0300

    bds

---
+++ bds/js/script.js
@@ -0,0 +1,8 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#nav-open').onclick = () => {
+  navbar.classList.add('active');
+}
+document.querySelector('#nav-close').onclick = () => {
+  navbar.classList.remove('active');
+}
