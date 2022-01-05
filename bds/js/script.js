be93f80793463e718f59d8c18d51eafa3c779e40
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-01-05 17:55:27 -0300

    fff

--- bds/js/script.js
+++ bds/js/script.js
@@ -6,3 +6,54 @@ document.querySelector('#nav-open').onclick = () => {
 document.querySelector('#nav-close').onclick = () => {
   navbar.classList.remove('active');
 }
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  if (window.scrollY > 0 ){
+    document.querySelector('.header').classList.add('active');
+  }else {
+    document.querySelector('.header').classList.remove('active');
+  }
+}
+window.onload = () => {
+  navbar.classList.remove('active');
+  if (window.scrollY > 0 ){
+    document.querySelector('.header').classList.add('active');
+  }else {
+    document.querySelector('.header').classList.remove('active');
+  }
+}
+var swiper = new Swiper(".clients-slider", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	autoplay: {
+		delay: 2000,
+	},
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 0,
+		depth: 0,
+		modifier: 1,
+		slideShadows: true,
+	},
+	loop:true,
+});
+
+var swiper = new Swiper(".swiper-container", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	autoplay: {
+		delay: 2000,
+	},
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 0,
+		depth: 0,
+		modifier: 1,
+		slideShadows: true,
+	},
+	loop:true,
+});
