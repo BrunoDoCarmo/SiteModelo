2383e815064ec0f49dda7ef806da340899f9c386
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-01-10 17:52:26 -0300

    sss

--- bds/js/script.js
+++ bds/js/script.js
@@ -1,59 +1,63 @@
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector('.header .navbar')
 
 document.querySelector('#nav-open').onclick = () => {
-  navbar.classList.add('active');
+  navbar.classList.add('active')
 }
 document.querySelector('#nav-close').onclick = () => {
-  navbar.classList.remove('active');
+  navbar.classList.remove('active')
 }
 window.onscroll = () => {
-  navbar.classList.remove('active');
-  if (window.scrollY > 0 ){
-    document.querySelector('.header').classList.add('active');
-  }else {
-    document.querySelector('.header').classList.remove('active');
+  navbar.classList.remove('active')
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active')
+  } else {
+    document.querySelector('.header').classList.remove('active')
   }
 }
 window.onload = () => {
-  navbar.classList.remove('active');
-  if (window.scrollY > 0 ){
-    document.querySelector('.header').classList.add('active');
-  }else {
-    document.querySelector('.header').classList.remove('active');
+  navbar.classList.remove('active')
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active')
+  } else {
+    document.querySelector('.header').classList.remove('active')
   }
 }
-var swiper = new Swiper(".clients-slider", {
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	autoplay: {
-		delay: 2000,
-	},
-	coverflowEffect: {
-		rotate: 0,
-		stretch: 0,
-		depth: 0,
-		modifier: 1,
-		slideShadows: true,
-	},
-	loop:true,
-});
+var swiper = new Swiper('.clients-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 2000
+  },
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: true
+  },
+  loop: true
+})
 
-var swiper = new Swiper(".swiper-container", {
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	autoplay: {
-		delay: 2000,
-	},
-	coverflowEffect: {
-		rotate: 0,
-		stretch: 0,
-		depth: 0,
-		modifier: 1,
-		slideShadows: true,
-	},
-	loop:true,
-});
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 2000
+  },
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: true
+  },
+  loop: true
+})
+let test = document.querySelector('.services .box-container')
+document.querySelector('.test').onclick = () => {
+  test.classList.add('active1')
+}
