681e1dec0b4d67d7e2e0433f5b27b9f3b5c6ba7d
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-02-01 17:06:07 -0300

    teste

---
+++ teste/salvacao 2/js/index.js
@@ -0,0 +1,41 @@
+// function ativaNoScroll() {
+//   document.querySelectorAll('img').forEach((img, index) => {
+//     if(img.getBoundingClientRect().top < window.innerHeight) {
+//       img.src = img.getAttribute('data-src')
+//     }
+//   })
+// }
+// window.addEventListener('scroll', ativaNoScroll);
+const debounce = function(func, wait, immediate) {
+  let timeout
+  return function(...args) {
+    const context = this
+    const later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+  target.forEach(function(el){
+    if((windowTop > el.offsetTop)){
+      el.classList.add(animationClass)
+    }else {
+      el.classList.remove(animationClass)
+    }
+  })
+}
+animeScroll();
+if (target.length){
+  window.addEventListener('scroll', debounce(function(){
+    animeScroll();
+  },200))
+}
